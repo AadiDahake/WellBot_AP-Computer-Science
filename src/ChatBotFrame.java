372e22bad9eda35d5739f2a99a0aec542d8a275a
Aadi Dahake <dahakeaadi@gmail.com>
2023-11-09 10:20:14 -0800

    Wrapper Added

--- src/ChatBotFrame.java
+++ src/ChatBotFrame.java
@@ -27,6 +27,8 @@ public class ChatBotFrame extends JFrame {
         chatPane.setBackground(chatAreaColor);
         chatPane.setForeground(textColor);
         chatPane.setFont(textFont);
+        chatPane.setEditorKit(new WrapEditorKit());
+
 
         JScrollPane scrollPane = new JScrollPane(chatPane);
         scrollPane.setBorder(BorderFactory.createEmptyBorder());
@@ -183,5 +185,51 @@ public class ChatBotFrame extends JFrame {
         SwingUtilities.invokeLater(() -> {
             new ChatBotFrame("ChatBot");
         });
+    }
+}
+
+class WrapEditorKit extends StyledEditorKit {
+    ViewFactory defaultFactory = new WrapColumnFactory();
+
+    public ViewFactory getViewFactory() {
+        return defaultFactory;
+    }
+
+    static class WrapColumnFactory implements ViewFactory {
+        public View create(Element elem) {
+            String kind = elem.getName();
+            if (kind != null) {
+                if (kind.equals(AbstractDocument.ContentElementName)) {
+                    return new WrapLabelView(elem);
+                } else if (kind.equals(AbstractDocument.ParagraphElementName)) {
+                    return new ParagraphView(elem);
+                } else if (kind.equals(AbstractDocument.SectionElementName)) {
+                    return new BoxView(elem, View.Y_AXIS);
+                } else if (kind.equals(StyleConstants.ComponentElementName)) {
+                    return new ComponentView(elem);
+                } else if (kind.equals(StyleConstants.IconElementName)) {
+                    return new IconView(elem);
+                }
+            }
+
+            return new LabelView(elem);
+        }
+    }
+
+    static class WrapLabelView extends LabelView {
+        public WrapLabelView(Element elem) {
+            super(elem);
+        }
+
+        public float getMinimumSpan(int axis) {
+            switch (axis) {
+                case View.X_AXIS:
+                    return 0;
+                case View.Y_AXIS:
+                    return super.getMinimumSpan(axis);
+                default:
+                    throw new IllegalArgumentException("Invalid axis: " + axis);
+            }
+        }
     }
 }
